e1a03c175e3fcb8848a1ab6e78d6704a67839b43
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2025-12-04 01:29:09 +0000

    peer connection

--- src/peer.rs
+++ src/peer.rs
@@ -1,6 +1,8 @@
-use crate::peer::peer::Value;
-use std::{collections::HashMap, sync::Arc};
+use crate::peer::peer::{PeerConnectionRequest, Value, peer_service_server::PeerService};
+use std::{collections::HashMap, pin::Pin, sync::Arc};
 use tokio::sync::{RwLock, mpsc};
+use tokio_stream::Stream;
+use tonic::{Request, Response, Status};
 
 pub mod peer {
     tonic::include_proto!("peer");
@@ -8,7 +10,7 @@ pub mod peer {
 
 #[derive(Debug)]
 pub struct PeerConnections {
-    pub peers: HashMap<i32, mpsc::Sender<String>>,
+    pub peers: HashMap<i32, mpsc::Sender<Value>>,
 }
 
 impl PeerConnections {
@@ -36,5 +38,66 @@ impl PeerConnections {
 
 #[derive(Debug)]
 pub struct PeerServerState {
-    pub state: Arc<RwLock<PeerConnections>>,
+    pub connections: Arc<RwLock<PeerConnections>>,
+    pub counter: Arc<RwLock<i32>>,
+}
+
+impl PeerServerState {
+    pub fn new(connections: Arc<RwLock<PeerConnections>>) -> Self {
+        Self {
+            connections,
+            counter: Arc::new(RwLock::new(0)),
+        }
+    }
+
+    pub async fn access_counter_and_increment(&self) -> i32 {
+        let count = *self.counter.read().await;
+        *self.counter.write().await = count + 1;
+        count
+    }
+}
+
+#[tonic::async_trait]
+impl PeerService for PeerServerState {
+    type ConnectToServerStream =
+        Pin<Box<dyn Stream<Item = Result<Value, Status>> + Send + Sync + 'static>>;
+
+    async fn connect_to_server(
+        &self,
+        _request: Request<PeerConnectionRequest>,
+    ) -> Result<Response<Self::ConnectToServerStream>, Status> {
+        let (stream_tx, stream_rx) = mpsc::channel(1);
+        let (tx, mut rx) = mpsc::channel(1);
+
+        let peer_index = {
+            let i = self.access_counter_and_increment().await;
+            self.connections.write().await.peers.insert(i.clone(), tx);
+            i
+        };
+
+        println!("A peer has connected.");
+
+        {
+            let shared_connections = self.connections.clone();
+            tokio::spawn(async move {
+                while let Some(msg) = rx.recv().await {
+                    match stream_tx.send(Ok(msg)).await {
+                        Ok(_) => {}
+                        Err(_) => {
+                            println!("Peer has disconnected.");
+                            shared_connections.write().await.peers.remove(&peer_index);
+                        }
+                    }
+                }
+            });
+        }
+
+        Ok(Response::new(Box::pin(
+            tokio_stream::wrappers::ReceiverStream::new(stream_rx),
+        )))
+    }
+
+    async fn exchange_value(&self, _request: Request<Value>) -> Result<Value, Status> {
+        todo!()
+    }
 }
